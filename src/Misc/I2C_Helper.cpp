cedab8606d77f974785b91d67ec8a42bd42263ae
Jamal-Ra-Davis <Jamal.Ra.Davis@gmail.com>
2022-09-21 23:04:32 -0700

    Add function for performing I2C update

--- src/Misc/I2C_Helper.cpp
+++ src/Misc/I2C_Helper.cpp
@@ -41,6 +41,17 @@ int i2c_write_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_val)
     };
     return i2c_write(dev_addr, wbuf, sizeof(wbuf));
 }
+int i2c_update_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_val, uint8_t mask)
+{
+    uint8_t old_val;
+    int ret = i2c_read_byte(dev_addr, reg_addr, &old_val);
+    if (ret < 0) {
+        return ret;
+    }
+
+    uint8_t new_val = (old_val & ~mask) | (reg_val & mask);
+    return i2c_write_byte(dev_addr, reg_addr, new_val);
+}
 int i2c_bus_scan(uint8_t *addr_list, uint8_t *addr_cnt, uint8_t max_addr_cnt)
 {
     int cnt = 0;
--- src/Misc/I2C_Helper.h
+++ src/Misc/I2C_Helper.h
@@ -6,4 +6,5 @@ int i2c_write_read(uint8_t dev_addr, uint8_t *wbuf, uint8_t wlen, uint8_t *rbuf,
 int i2c_write(uint8_t dev_addr, uint8_t *wbuf, uint8_t wlen);
 int i2c_read_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_val);
 int i2c_write_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_val);
+int i2c_update_byte(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_val, uint8_t mask);
 int i2c_bus_scan(uint8_t *addr_list, uint8_t *addr_cnt, uint8_t max_addr_cnt);
